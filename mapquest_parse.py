5f26eff656a78b4133b17589e4d4b47391dd2ee7
Kay Lab <kaylab@air-08.local>
2015-03-12 23:50:15 -0700

    Created functions to parse for directions, distance, time, and lat / long

--- mapquest_parse.py
+++ mapquest_parse.py
@@ -1,24 +1,30 @@
 # Project 3: MapQuest API Example Parse JSON
 
-import http.client
 import urllib.request
 import urllib.error
 import urllib.parse 
+import json
 
-def format_url(base_url:str, start_loc:str, end_loc:str)->str:
+APP_KEY = 'Fmjtd%7Cluu82luanl%2C72%3Do5-948x04'
+URL = 'http://open.mapquestapi.com/directions/v2/route?key={}&'.format(APP_KEY)
+
+def format_url(start_loc:str, end_loc:str)->str:
     '''Takes in a start and end location and returns a url string formatted
     with the inputs'''
-    url_encode = urllib.parse.urlencode([('from', start_loc), ('to', end_loc)])
-    return (base_url + url_encode)
+    query_parameters = [('from', start_loc), ('to', end_loc)]
+    url_encode = urllib.parse.urlencode(query_parameters)
+    return (URL + url_encode)
     
-
-def download_url(url:str)->None:
-    '''Takes in a url and prints out the contents of the web page'''
+def get_result(url:str)->'json':
+    '''Takes in a url and returns a Python object representing the parsed JSON
+    result'''
     response = None
     try:
         response = urllib.request.urlopen(url)
-        content = response.read()
-        print(content)
+        # Converts response to JSON format 
+        json_text = response.read().decode(encoding = 'utf-8')
+        # Converts into Python object 
+        return json.loads(json_text)
     except urllib.error.HTTPError as e:
         print('Failed to download contents of URL')
         print('Status code: {}'.format(e.code))
@@ -26,3 +32,44 @@ def download_url(url:str)->None:
     finally:
         if response != None:
             response.close()
+
+def print_steps(json_result:'json')->None:
+    '''Takes a parsed JSON response and prints the directions / steps'''
+    step_list = json_result['route']['legs']
+    for i in step_list:
+        for j in i['maneuvers']:
+            print(j['narrative'])
+
+def print_distance(json_result:'json')->None:
+    '''Takes a parsed JSON response and prints the total distance'''
+    print(json_result['route']['distance'])
+
+def print_time(json_result:'json')->None:
+    '''Takes a parsed JSON response and prints the total time'''
+    seconds = json_result['route']['time']
+    print(seconds/60)
+
+def map_direction(parameter:str, n:float)->str:
+    '''Takes in a latitude or longitude value and determines the direction'''
+    if parameter == 'lat':
+        if n <= 90:
+            return '{}N'.format(n)
+        return '{}S'.format(n)
+    elif parameter == 'long':
+        if n <= 90:
+            return '{}E'.format(n)
+        return '{}W'.format(n)
+
+def print_latlong(json_result:'json')->None:
+    '''Takes a parsed JSON response and prints the latitude and longitude'''
+    coord = json_result['route']['boundingBox']
+    start_lat = abs(coord['lr']['lat'])
+    start_long = abs(coord['lr']['lng'])
+    print(map_direction('lat', start_lat), map_direction('long', start_long))
+    end_lat = abs(coord['ul']['lat'])
+    end_long = abs(coord['ul']['lng'])
+    print(map_direction('lat', end_lat), map_direction('long', end_long))
+    
+    
+    
+            
--- mapquest_ui.py
+++ mapquest_ui.py
@@ -3,19 +3,24 @@
 import mapquest_parse
 
 def main():
-    APP_KEY = 'Fmjtd%7Cluu82luanl%2C72%3Do5-948x04'
-    URL = 'http://open.mapquestapi.com/directions/v2/route?key={}&'.format(APP_KEY)
-    
+
     inputs = int(input())
     location_list = input_list(inputs)
     outputs = int(input())
     output_list = input_list(outputs)
 
     for i in range(len(location_list)-1):
-        url = mapquest_parse.format_url(URL, location_list[i], location_list[i+1])
+        url = mapquest_parse.format_url(location_list[i], location_list[i+1])
         print(url)
         print()
-        mapquest_parse.download_url(url)
+        json_result = mapquest_parse.get_result(url)
+        mapquest_parse.print_steps(json_result)
+        print()
+        mapquest_parse.print_distance(json_result)
+        print()
+        mapquest_parse.print_time(json_result)
+        print()
+        mapquest_parse.print_latlong(json_result)
 
 def input_list(n:int)->list:
     '''Returns a list of n locations taken in as input'''
